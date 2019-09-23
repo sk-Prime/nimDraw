a40c16fff831253d0cd40e92afdf7d1665f8caf6
Sk <sksalim000@yahoo.com>
2019-09-23 11:29:03 +0800

    Add files via upload

---
+++ nimDraw/tests/spiral.nim
@@ -0,0 +1,7 @@
+import nimDraw
+
+var turtle = newWalker((400,400),(300,50),length= 20, angle = 90)
+for i in 0..36:
+  turtle.walk()
+  turtle.angle += i
+discard turtle.canvas.saveAsPng("spiral.png")
